9639466653f4203f9269778e4368b8764e491010
Dick Childress <dick.childress@icearp.net>
2020-03-05 14:23:29 -0500

    Remove comment blocks from ec2-sg.tf.

--- ec2-sg.tf
+++ ec2-sg.tf
@@ -12,21 +12,6 @@ resource "aws_security_group" "allow_cloudfront_global_ips" {
     },
     var.input_tags
   )
-/*
-  count = length(var.permitted_protocols)
-
-  name        = "${var.group_name_global}-${element(var.permitted_protocols, count.index)}"
-  description = "Allow ingress ${element(var.permitted_protocols, count.index)} traffic from Amazon CloudFront global IP ranges."
-  vpc_id      = data.aws_vpc.selected.id
-  tags = merge(
-    {
-      Name        = "cloudfront_r"
-      AutoUpdate  = "true"
-      Protocol    = "${element(var.permitted_protocols, count.index)}"
-    },
-    var.input_tags
-  )
-*/
 }
 
 resource "aws_security_group" "allow_cloudfront_regional_ips" {
@@ -43,19 +28,4 @@ resource "aws_security_group" "allow_cloudfront_regional_ips" {
     },
     var.input_tags
   )
-/*
-  count = length(var.permitted_protocols)
-
-  name        = "${var.group_name_regional}-${element(var.permitted_protocols, count.index)}"
-  description = "Allow ingress ${element(var.permitted_protocols, count.index)} traffic from Amazon CloudFront regional IP ranges."
-  vpc_id      = data.aws_vpc.selected.id
-  tags = merge(
-    {
-      Name        = "cloudfront_r"
-      AutoUpdate  = "true"
-      Protocol    = "${element(var.permitted_protocols, count.index)}"
-    },
-    var.input_tags
-  )
-*/
 }
